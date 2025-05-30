f89bde9929a51cc20479a9227c008e845cf50fb5
likhith lanka <likhithsai.l15@iiits.in>
2025-05-31 04:20:12 +0530

    Test Generate Insights

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import BookmarksDashboard from './BookmarksDashboard';
+import LearningInsights from './LearningInsights';
 
 const Index = () => {
-  return <BookmarksDashboard />;
+  return (
+    <Tabs defaultValue="dashboard" className="w-full">
+      <TabsList className="w-full justify-start">
+        <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+        <TabsTrigger value="insights">Learning Insights</TabsTrigger>
+      </TabsList>
+      <TabsContent value="dashboard">
+        <BookmarksDashboard />
+      </TabsContent>
+      <TabsContent value="insights">
+        <LearningInsights />
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default Index;
