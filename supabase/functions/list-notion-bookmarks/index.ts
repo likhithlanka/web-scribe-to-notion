3409034cc3f4613bf689465146ac599894d60f47
likhith lanka <likhithsai.l15@iiits.in>
2025-05-31 02:26:45 +0530

    Authorization issue

--- supabase/functions/list-notion-bookmarks/index.ts
+++ supabase/functions/list-notion-bookmarks/index.ts
@@ -1,6 +1,6 @@
-import "https://deno.land/x/xhr@0.1.0/mod.ts";
 // @ts-ignore
 Deno.env.set("SUPABASE_FUNCTIONS_VERIFY_JWT", "false");
+import "https://deno.land/x/xhr@0.1.0/mod.ts";
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
 const corsHeaders = {
