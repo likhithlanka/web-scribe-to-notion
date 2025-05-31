c9635b99d03d3f081df3dbc6b8ac588a4325d0f6
likhith lanka <likhithsai.l15@iiits.in>
2025-05-31 06:19:41 +0530

    Update Activity Widget for full width

--- src/pages/widgets/activity.tsx
+++ src/pages/widgets/activity.tsx
@@ -2,8 +2,8 @@ import { ActivityWidget } from "@/components/widgets/ActivityWidget";
 
 export default function ActivityPage() {
   return (
-    <div className="min-h-screen w-screen p-4 flex items-center justify-center bg-gray-50">
-      <div className="w-full max-w-2xl aspect-[2/1]">
+    <div className="min-h-screen w-screen p-2 flex items-center justify-center bg-gray-50">
+      <div className="w-full max-w-[1200px]">
         <ActivityWidget />
       </div>
     </div>
