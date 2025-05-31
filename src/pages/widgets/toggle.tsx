599778d78c0af388c7a94c4455ad6aec43e6ec22
likhith lanka <likhithsai.l15@iiits.in>
2025-05-31 15:11:14 +0530

    Updated toggle.tsx

--- src/pages/widgets/toggle.tsx
+++ src/pages/widgets/toggle.tsx
@@ -2,7 +2,7 @@ import { ToggleWidget } from "@/components/widgets/ToggleWidget";
 
 export default function TogglePage() {
   return (
-    <div className="min-h-screen w-screen p-4 flex items-center justify-center bg-[#F7F7F7] dark:bg-[#1F1F1F]">
+    <div className="min-h-screen w-screen p-4 flex items-center justify-center bg-gray-50">
       <div className="w-full max-w-[1400px] h-[600px]">
         <ToggleWidget />
       </div>
